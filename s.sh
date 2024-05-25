1a1b77aad9550e375beb57d657f140e0fab8b014
Tiffany Li <tiffanysiyangli@gmail.com>
2024-05-24 21:33:24 -0400

    update 1

--- s.sh
+++ s.sh
@@ -3,22 +3,20 @@
 # Change to your project's directory
 cd /c/Users/flyku/Documents/GitHub/open-alteryx-macro
 
-
 # Get the current date as the number of seconds since 1970-01-01 00:00:00 UTC
 now=$(date +%s)
 
 # Two years ago in seconds
 two_years_ago=$(date -d"2 years ago" +%s)
 
-# Number of commits to make
-num_commits=100
+# Predefined commit messages to mimic real-world commit messages
+commit_messages=("Initial commit" "Update" "Fix bug" "Refactor code" "Add new feature" "Improve performance" "Update documentation")
 
-for ((i=0; i<$num_commits; i++)); do
-  # Select a random file in the repository
-  file=$(find . -type f | shuf -n 1)
-
-  # Append a random string to the file
-  echo "Random string $(date +%N)" >> "$file"
+# Iterate over each directory, excluding .git
+find . -type d -not -path './.git*' | while read dir; do
+  # Create a new file in the directory
+  file="$dir/new_file_$(date +%N).txt"
+  echo "This is a new file." > "$file"
 
   # Stage the file for commit
   git add "$file"
@@ -26,10 +24,9 @@ for ((i=0; i<$num_commits; i++)); do
   # Generate a random date between now and two years ago
   commit_date=$(date -d"@$((RANDOM % (now - two_years_ago) + two_years_ago))" --iso-8601=seconds)
 
-  # Commit the change with the random date
-  GIT_AUTHOR_DATE="$commit_date" GIT_COMMITTER_DATE="$commit_date" git commit -m "Random commit"
+  # Select a random commit message from the predefined list
+  commit_message=${commit_messages[$RANDOM % ${#commit_messages[@]}]}
 
+  # Commit the change with the random date and the selected commit message
+  GIT_AUTHOR_DATE="$commit_date" GIT_COMMITTER_DATE="$commit_date" git commit -m "$commit_message"
 done
-
-# Push the changes to the remote repository
-git push origin master
